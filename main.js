cbbf3143116dc0db1457234c81d1079bbaf76b8c
Max Mikkola <max.mikkola@futurice.com>
2016-04-19 15:08:26 +0300

    Options and livereloading keywords

--- main.js
+++ main.js
@@ -1,4 +1,18 @@
+
 var observer;
+let filterWhiteList = []
+
+
+chrome.storage.sync.get({keywords: "kappa"}, ({keywords}) => {
+  filterWhiteList = keywords.split(",")
+
+})
+
+chrome.storage.onChanged.addListener(function(changes, namespace) {
+  filterWhiteList = changes["keywords"].newValue.split(",")
+  window.speechSynthesis.speak(new SpeechSynthesisUtterance("New keywords!"))
+})
+
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     if (request.message === "clicked_browser_action") {
@@ -6,16 +20,31 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
           const comments = document.querySelectorAll('ul.chat-lines li')
 
           const length = comments.length
-          const comment = comments[comments.length - 1].querySelector('.message')
-          const regex = /(\<.​* alt="([a-zA-Z]*​)".*\>)/g
+          const comment = comments[comments.length - 1].querySelector('.message').innerHTML
+          const regex = /(\<.* alt="([a-zA-Z]*)" .*\>*)/gi
 
-          window.speechSynthesis.speak(new SpeechSynthesisUtterance(comment.innerHTML.replace(regex, '$2')))
+          const commentText = comment
+            .replace(/\\n/g, "")
+            .trim().replace(regex, (a,b,c) => {
+              return c.trim()
+            }).replace(/\<.*\>/g, "").replace("***", "fuck").toLowerCase()
+
+
+
+          const isOnWhiteList = filterWhiteList.filter(whiteWord => commentText.includes(whiteWord)).length
+          console.log(commentText)
+          if(isOnWhiteList) {
+            console.log(commentText)
+            window.speechSynthesis.speak(new SpeechSynthesisUtterance(commentText))
+          }
         }
 
         if (!observer) {
+            console.log('Starting observer')
             observer = new MutationObserver(speak)
             observer.observe(document.querySelector('ul.chat-lines'), {childList: true})
         } else {
+            console.log('disconnecting')
             observer.disconnect()
             window.speechSynthesis.cancel()
             observer = null
@@ -23,3 +52,5 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 
     }
 })
+
+
